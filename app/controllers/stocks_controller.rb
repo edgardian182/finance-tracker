8c6d26a048b452e098eca3d6038b68c00f134996
Andres <edgarquintana984@hotmail.com>
2016-04-20 13:00:56 +0000

    Agregada Asociacion User-Stock M-t-M y boton para agregar acciones

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,8 +3,8 @@ class StocksController < ApplicationController
   def search
     
     if params[:stock]
-      @stock = Stock.find_by_ticker(params[:stock])
-      @stock ||= Stock.new_from_lookup(params[:stock])
+      @stock = Stock.find_by_ticker(params[:stock])  # Busca primero en la DB propia
+      @stock ||= Stock.new_from_lookup(params[:stock]) # Si no encuentra busca directament en la gema
     end
     
     if @stock
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,13 @@
 class Stock < ActiveRecord::Base
   
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+  
   def self.find_by_ticker(ticker_symbol)
-    where(ticker: ticker_symbol).first
+    where(ticker: ticker_symbol).first   # Busca en la DB propia las stocks que se hayan guardado
   end
   
-  def self.new_from_lookup(ticker_symbol)
+  def self.new_from_lookup(ticker_symbol)  # Busca en la gema la stock y le crea
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
     return nil unless looked_up_stock.name
     
